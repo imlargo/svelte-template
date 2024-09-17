fc05bfcfe1bb646253fb736bc5934efa9b6c6b47
imlargo <jlargob@unal.edu.co>
2024-09-17 15:28:06 -0500

    fix slot in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,11 @@
-<script>
+<script lang="ts">
 	import '$styles/app.scss';
+
+	import type { Snippet } from 'svelte';
+	type Props = {
+		children: Snippet;
+	};
+	const { children }: Props = $props();
 </script>
+
+{@render children()}
