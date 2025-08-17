8f0bb1676baacaaead3b4f01f4e6ba568737b0c1
imlargo <jlargob@unal.edu.co>
2025-08-17 17:02:21 -0500

    Add alias configuration to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$lib: './src/lib',
+			$styles: './src/styles',
+			$components: './src/lib/components',
+			$client: './src/lib/client',
+			$server: './src/lib/server',
+			$stores: './src/lib/stores',
+		}
 	}
 };
 
